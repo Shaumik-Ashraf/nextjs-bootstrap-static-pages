ab6afb47d012e18fabae1041acee6af4bca69dde
Shaumik-Ashraf <a.shaumik@gmail.com>
2025-10-26 22:39:49 -0400

    debugging ci

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
 		return(
 			<footer className="text-center text-muted">
 				<hr />
-				<p>&copy; 2025 | Version {process.env.VERSION}</p>
+				<p>&copy; 2025</p>
 			</footer>
 		)
 }
