60db38d5569d46d1e46516643faa5cfe4150bde1
Shaumik-Ashraf <a.shaumik@gmail.com>
2025-10-26 21:28:34 -0400

    debugging

--- next.config.js
+++ next.config.js
@@ -1,34 +1,18 @@
-import { PHASE_DEVELOPMENT_SERVER } from 'next/constants'
+const today = new Date()
+const version = `${today.getFullYear()}.${today.getMonth()+1}.${today.getDate()}`
+const nextConfig = {
+  output: 'export',
  
-module.exports = (phase, { defaultConfig }) => {
-  const date = new Date()
-	const version = `${date.getFullYear()}.${date.getMonth()+1}.${date.getDate()}`
-  const nextConfig = {
-    // config options for all phases here
-    output: 'export',
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+	// distDir: 'dist',
 
-    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    trailingSlash: false,
- 
-    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-    // skipTrailingSlashRedirect: true,
- 
-		env: {
-		  "VERSION": `${version}-dev`
-		}
-	}
-
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return Object.assign(nextConfig, {
-      // development only config options here
-      allowedDevOrigins: ['127.0.0.1', '::1'],
-    })
+  env: {
+    'VERSION': `${version}`
   }
- 
-  return Object.assign(nextConfig, {
-    // config options for export here
-    env: {
-      "VERSION": version
-    }
-  })
 }
